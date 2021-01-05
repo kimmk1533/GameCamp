bdc86961c8b860c581cb01ebaa46684464d98d50
조진서 <oness68@users.noreply.github.com>
2021-01-06 06:15:58 +0900

    오브젝트클릭테스트

---
+++ Assets/MyAsset/03_Scripts/obj_click.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class obj_click : MonoBehaviour
+{
+
+    GameObject target;
+    [SerializeField]
+    GameObject boad = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            CastRay();
+        }
+    }
+
+    void CastRay()
+    {
+        target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+        if (hit.collider != null && hit.collider.transform == this.transform)
+        {
+            target = hit.collider.gameObject;
+            Debug.Log(this.gameObject.name);
+            boad.SetActive(true);
+        }
+        else
+        {
+            boad.SetActive(false);
+        }
+    }
+
+
+}
