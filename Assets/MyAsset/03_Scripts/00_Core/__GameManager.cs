002f717289e46b9d75751cb68b192cfc2ad7f78e
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2021-01-05 16:25:11 +0900

    게임매니저 위치 변경

---
+++ Assets/MyAsset/03_Scripts/00_Core/__GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class __GameManager : Singleton<__GameManager>
+{
+    public override void __Initialize()
+    {
+        
+    }
+}
