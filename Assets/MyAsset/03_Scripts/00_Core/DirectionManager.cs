89af05f7907b9e2fbdaabd037585ce554d3ec4ff
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2021-01-05 18:23:36 +0900

    방향 전환 페이드 추가

--- Assets/MyAsset/03_Scripts/00_Core/DirectionManager.cs
+++ Assets/MyAsset/03_Scripts/00_Core/DirectionManager.cs
@@ -21,8 +21,16 @@ public class DirectionManager : Singleton<DirectionManager>
 
     public override void __Initialize()
     {
+        // 이동시킬 카메라
+        if (m_Camera == null)
+        {
+            m_Camera = Camera.main;
+        }
+
+        // 현재 방향
         m_Dir = E_Direction.North;
 
+        // 방향에 따른 위치들
         m_Positions = new Dictionary<E_Direction, Vector3>();
 
         m_Positions.Add(E_Direction.North, new Vector3(-30f, 0f, -50f));
@@ -30,20 +38,21 @@ public class DirectionManager : Singleton<DirectionManager>
         m_Positions.Add(E_Direction.South, new Vector3(10f, 0f, -50f));
         m_Positions.Add(E_Direction.West, new Vector3(30f, 0f, -50f));
 
+        // 카메라 이동
+        CameraMove();
+    }
+
+    public void CameraMove()
+    {
         m_Camera.transform.position = m_Positions[m_Dir];
         Debug.Log(m_Dir);
     }
-
     public void TurnRight()
     {
         m_Dir = (E_Direction)((int)(m_Dir + 1) % (int)E_Direction.Max);
-        Debug.Log(m_Dir);
-        m_Camera.transform.position = m_Positions[m_Dir];
     }
     public void TurnLeft()
     {
         m_Dir = (E_Direction)((int)(m_Dir + (int)E_Direction.Max - 1) % (int)E_Direction.Max);
-        Debug.Log(m_Dir);
-        m_Camera.transform.position = m_Positions[m_Dir];
     }
 }
---
+++ Assets/MyAsset/03_Scripts/Fade.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    DirectionManager m_Direction;
+    Animator m_Animator;
+    bool m_IsFade;
+
+    public void Awake()
+    {
+        __Initialize();
+    }
+    public void __Initialize()
+    {
+        m_Direction = DirectionManager.Instance;
+        m_Animator = GetComponent<Animator>();
+        m_IsFade = false;
+
+        LeftButtonClick += TurnLeft;
+        RightButtonClick += TurnRight;
+    }
+
+    public void CameraMove()
+    {
+        m_Direction.CameraMove();
+    }
+
+    public delegate void ButtonClick();
+
+    public ButtonClick LeftButtonClick;
+    public ButtonClick RightButtonClick;
+
+    void FadeStart()
+    {
+        m_IsFade = true;
+    }
+    void FadeEnd()
+    {
+        m_IsFade = false;
+    }
+    void TurnLeft()
+    {
+        m_Direction.TurnLeft();
+        m_Animator.SetTrigger("Fade");
+    }
+    void TurnRight()
+    {
+        m_Direction.TurnRight();
+        m_Animator.SetTrigger("Fade");
+    }
+    public void OnLeftButtonClick()
+    {
+        if (!m_IsFade)
+        {
+            LeftButtonClick?.Invoke();
+        }
+    }
+    public void OnRightButtonClick()
+    {
+        if (!m_IsFade)
+        {
+            RightButtonClick?.Invoke();
+        }
+    }
+}
