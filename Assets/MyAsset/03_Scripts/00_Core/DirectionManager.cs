947808141310359955282d3329069fd2b5fe0d51
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2021-01-06 14:09:09 +0900

    방향 버튼 마우스 위치 페이드 추가

--- Assets/MyAsset/03_Scripts/00_Core/DirectionManager.cs
+++ Assets/MyAsset/03_Scripts/00_Core/DirectionManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum E_Direction
 {
@@ -14,21 +15,19 @@ public enum E_Direction
 
 public class DirectionManager : Singleton<DirectionManager>
 {
-    public Camera m_Camera;
-    public E_Direction m_Dir;
+    public E_Direction m_CurrentDir;
+    public List<Image> m_DirImages;
 
+    Camera m_Camera;
     Dictionary<E_Direction, Vector3> m_Positions;
 
     public override void __Initialize()
     {
         // 이동시킬 카메라
-        if (m_Camera == null)
-        {
-            m_Camera = Camera.main;
-        }
+        m_Camera = Camera.main;
 
         // 현재 방향
-        m_Dir = E_Direction.North;
+        m_CurrentDir = E_Direction.North;
 
         // 방향에 따른 위치들
         m_Positions = new Dictionary<E_Direction, Vector3>();
@@ -44,15 +43,29 @@ public class DirectionManager : Singleton<DirectionManager>
 
     public void CameraMove()
     {
-        m_Camera.transform.position = m_Positions[m_Dir];
-        Debug.Log(m_Dir);
+        m_Camera.transform.position = m_Positions[m_CurrentDir];
+        Debug.Log(m_CurrentDir);
     }
     public void TurnRight()
     {
-        m_Dir = (E_Direction)((int)(m_Dir + 1) % (int)E_Direction.Max);
+        m_CurrentDir = (E_Direction)((int)(m_CurrentDir + 1) % (int)E_Direction.Max);
     }
     public void TurnLeft()
     {
-        m_Dir = (E_Direction)((int)(m_Dir + (int)E_Direction.Max - 1) % (int)E_Direction.Max);
+        m_CurrentDir = (E_Direction)((int)(m_CurrentDir + (int)E_Direction.Max - 1) % (int)E_Direction.Max);
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+
+        foreach (var item in m_DirImages)
+        {
+            Vector2 itemPos = item.transform.position;
+
+            Color temp = item.color;
+            temp.a = 1 - Mathf.Clamp01(Vector2.Distance(itemPos, mousePos));
+            item.color = temp;
+        }
     }
 }
