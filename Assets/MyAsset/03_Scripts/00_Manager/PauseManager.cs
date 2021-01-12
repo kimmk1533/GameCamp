9cdff9a7f8a3265a86482c2bf6058d626fb7c2c4
조진서 <oness68@users.noreply.github.com>
2021-01-13 01:30:29 +0900

    다운로드

--- Assets/MyAsset/03_Scripts/00_Manager/PauseManager.cs
+++ Assets/MyAsset/03_Scripts/00_Manager/PauseManager.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public enum WindowMode
